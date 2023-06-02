74b0b5ee8871c8be91834d465e6fc27e09e2c738
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-06-02 19:14:28 +0500

    B

--- app/(components)/portfolioCards/portfolioCards.js
+++ app/(components)/portfolioCards/portfolioCards.js
@@ -3,7 +3,7 @@
 export default function portfolioCards(props) {
   return (
     <div className=" sm:text-center">
-    <embed src={props.src} type="" className="rounded-lg" />
+    <embed src={props.src} type="" className="rounded-lg hover:shadow-2xl" />
     </div>
   )
 }
