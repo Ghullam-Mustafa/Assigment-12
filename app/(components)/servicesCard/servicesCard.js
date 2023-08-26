505785ad9a395ae72eb5d4ea29a1aad4fe8b0190
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-08-26 23:53:13 +0500

    v

---
+++ app/(components)/servicesCard/servicesCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+export default function servicesCard(props) {
+    return (
+        <>
+            <div className="border-[#2370C8] border-2 text-center shadow-lg  p-5">
+                <div className="text-center align-middle ">
+                <Image src={props.src}  width={50} height={50} className=' mx-auto' />
+                </div>
+                <p className='text-center text-2xl'>{props.title}  </p>
+                <p className='text-center text-[#646464] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmodtempor incididunt ut labore et dolore</p>
+            </div>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,26 +1,41 @@
 import Navbar from "./(components)/navbar/navbar"
 import Image from "next/image"
+import Card from './(components)/servicesCard/servicesCard'
 export default function Home() {
   return (
     <>
 
       <div className="header">
         <Navbar />
-        <div className="mx-auto w-10/12 mt-2 grid pb-10 lg:grid-cols-2 gap-5">
-        <div className="mt-20  lg:hidden ">
+        <div className="mx-auto container w-10/12 mt-2 grid pb-10 lg:grid-cols-2 mg:grid-cols-2 sm:grid-cols-1   gap-5">
+          <div className="mt-20  lg:hidden  ">
             <Image src='/Frame.png' width={5000}  height={5000} />
           </div>
-          <div className="lg:mt-32  mt-10  ">
-            <p className="text-white text-7xl  font-medium w-11/12 ">Creative Digital  Agency</p>
-            <p className="text-white py-5 w-11/12 text-sm font-normal leading-7 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+          <div className="lg:mt-32  mt-10   ">
+            <p className="text-white text-7xl  font-medium  ">Creative Digital  Agency</p>
+            <p className="text-white py-5  text-sm font-normal leading-7 ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
 
             <button type="button" className="text-white  bg-[#2370C8] px-10 py-2 rounded-full text-center justify-center  "  >Learn More</button>
           </div>
-          <div className="mt-20  hidden lg:block">
+          <div className="mt-20  hidden lg:block  ">
             <Image src='/Frame.png' width={5000} height={5000} />
           </div>
         </div>
       </div>
+
+      <div className="secondSection bg-[#FFFFFF]">
+         <div className="services lg:mt-20"> 
+            <p className="text-[#2370C8] text-center">OUR SERVICES</p>
+            <p className="text-4xl text-center">We Are Providing Digital services</p>
+            <div className=" w-10/12  mx-auto grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-1 gap-10 my-10 ">
+              <div className=""><Card  src='/web.webp' title='Web App Development' /> </div>
+              <div className=""><Card  src='/mobile.webp' title='Mobile App Development' /> </div>
+              <div className=""><Card  src='/ai.webp' title='AI Based Solutions' /> </div>
+            
+            </div>
+         </div>
+      </div>
+      
     </>
   )
 }
